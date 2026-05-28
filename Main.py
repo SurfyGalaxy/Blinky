f5d40c7c0d8de626e9358bf13313fabaaaf6d5f9
SurfyGalaxy <ashton3141@gmail.com>
2026-05-29 03:06:03 +1000

    Bugfixes

--- Main.py
+++ Main.py
@@ -1,4 +1,3 @@
-# 41 (till i use the script)
 import requests
 import datetime
 import json
@@ -9,28 +8,36 @@ names = []
 scope = []
 new = None
 looping = True
+loaded = False
 
 while looping == True:
-    file = input("Load an existing YSWS profile? (Enter a YSWS name, or press Enter to skip)")
-    file = f"{file.lower().replace(' ', '_')}_config.json"
+    file_input = input("Load an existing YSWS profile? (Enter a YSWS name, or press Enter to skip): ")
+    
+    if file_input == "":
+        looping = False
+        break
+    
+    file = f"{file_input.lower().replace(' ', '_')}_config.json"
+    
     if file in os.listdir():
         looping = False
         with open(file, 'r') as f:
-            data = json.load(f)
-        username = data["username"]
-        target = data["target_hours"] * 3600
-        days = data["days"]
-        scope = data["projects"]
+            loaded_data = json.load(f)
+            username = loaded_data["username"]
+            target = loaded_data["target_hours"] * 3600
+            start_days = loaded_data["days"]
+            scope = loaded_data["projects"]
+
         response = requests.get(f"https://hackatime.hackclub.com/api/v1/users/{username}/stats?features=projects,languages")
-        data = response.json()
-        stats = data["data"]
-        new = 0
+        api_data = response.json()
+        stats = api_data["data"]
+        loaded = True
     elif file == "_config.json":
         looping = False
     else:
         print("Invalid YSWS name. Did you save it last time, or mispell it?")
 
-if new == None:
+if loaded == False:
     looping = True
     while looping == True:
         username = input("Who's stats are we using here? ")
@@ -43,15 +50,17 @@ if new == None:
     if response.status_code == 200:
         data = response.json()
         stats = data["data"]
-    for proj in stats["projects"]:
-        if proj["total_seconds"] != 0:
-            names.append(proj["name"])
+
+for proj in stats["projects"]:
+    if proj["total_seconds"] != 0:
+        names.append(proj["name"])
 days_left = int(input("How many days left do you have? "))
 
 looping = True
 while looping == True:
+    available_projects = [proj for proj in names if proj not in scope]
     new = input(f"""Which projects are you adding? Or type 'exit' to proceed, or 'remove' to remove a project added earlier.
-     Available projects: {names}, Added projects: {scope} \n""")
+     Available projects: {available_projects}, Added projects: {scope} \n""")
 
     if new not in ["exit", "remove"]:
         if new in names:
@@ -90,21 +99,27 @@ percent_done = (total_time/target)*100
 print(f"Percent complete: {round(float(percent_done))}%")
 
 
-if input("Do you want me to save these settings as a recallable file, to allow further tracking? (True/False)") == "True":
-    ysws_name = input("What's the name of this YSWS? ")
-    start_days = input(f"How many days did you have when starting? Or leave blank to use {days_left} days ")
-    if not start_days:
-        start_days = days_left
+if input("Do you want me to save/update these settings as a recallable file, to allow further tracking? (True/False) ") == "True":
+    if loaded == False:
+        ysws_name = input("What's the name of this YSWS? ")
+        start_days_input = input(f"How many days did you have when starting? Or leave blank to use {days_left} days ")
+        if not start_days_input:
+            start_days = days_left
+        else:
+            start_days = int(start_days_input)
+        filename = f"{ysws_name.lower().replace(' ', '_')}_config.json"
     else:
-        start_days = int(start_days)
+        filename = file
     
-    data = {
+    save_data = {
         "username": username,
         "target_hours": target // 3600,
         "days": start_days,
         "projects": scope
     }
-    filename = f"{ysws_name.lower().replace(' ', '_')}_config.json"
+    
+    if os.path.exists(filename) and loaded == True:
+        os.remove(filename)
     with open(filename, 'w') as f:
-        json.dump(data, f)
+        json.dump(save_data, f)
     print("Done!")
